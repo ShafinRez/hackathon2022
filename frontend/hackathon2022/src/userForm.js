fa6aaa37c4f83226cb44ba04afa9ee3fe6d90ed8
MAHIMA DHAWAN <mdhawan@ualberta.ca>
2022-01-15 16:23:35 -0700

    form error #2

--- frontend/hackathon2022/src/userForm.js
+++ frontend/hackathon2022/src/userForm.js
@@ -1,69 +1,101 @@
+//reference: https://onestepcode.com/creating-a-material-ui-form/
+
 import React, { useState } from "react";
-import { Grid} from '@material-ui/core';
+import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import Slider from "@material-ui/core/Slider";
+import Button from "@material-ui/core/Button";
 
 
-const initialFValues = {
-    id: 0,
-    availability: '',
-    fullName: '',
-    email: '',
-    mobile: '',
-    task: '',
-    deadline: new Date(),
-    isDone: false,
-    hoursneeded: '',
-}
+const defaultValues = {
+  task: "",
+  numberofHours: 0,
+  priority: "",
+};
+
 export default function userForm(){
-	const [values, setValues] = useState({
-                                            id: 0,
-                                            availability: '',
-                                            fullName: '',
-                                            email: '',
-                                            mobile: '',
-                                            task: '',
-                                            deadline: new Date(),
-                                            isDone: false,
-                                            hoursneeded: '',
+  const [formValues, setFormValues] = useState(defaultValues);
+  const handleInputChange = (e) => { const { name, value } = e.target;
+                                        setFormValues({
+                                            ...formValues,
+                                            [name]: value,
                                         });
+                                        };
 
-	return(
-	    <form>
-        <Grid container>
-                <Grid item xs={6}>
-                    <TextField
-                    variant = "outlined"
-                    label = "fullName"
-                    value = {values.fullName}
-                    />
-                    <TextField
-                    variant = "outlined"
-                    label = "email"
-                    value = {values.email}
-                    />
-                    <TextField
-                    label="Mobile"
-                    name="mobile"
-                    value={values.mobile}
-                    />
-                    <TextField
-                    label="Task"
-                    name="task"
-                    value={values.task}
-                    />
-                    <TextField
-                    label="availability"
-                    name="availability"
-                    value={values.availability}
-                    />
-                    <TextField
-                    label="hoursneeded"
-                    name="hoursneeded"
-                    value={values.hoursneeded}
-                    />
-                    </Grid>
-                <Grid item xs={6}></Grid>
-                </Grid>     
-	            </form> 
-	)
-}
+  const handleSliderChange = (name) => (e, value) => {
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formValues);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <Grid container alignItems="center" justify="center" direction="column">
+        <Grid item>
+          <TextField
+            id="task-input"
+            name="task"
+            label="Task Input"
+            type="text"
+            value={formValues.task}
+            onChange={handleInputChange}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="numberofHours-input"
+            name="numberofHours"
+            label="Number of hours required"
+            type="number"
+            value={formValues.numberofHours}
+            onChange={handleInputChange}
+          />
+        </Grid>
+        <Grid item>
+          <FormControl>
+            <FormLabel>Priority</FormLabel>
+            <RadioGroup
+              name="priority"
+              value={formValues.priority}
+              onChange={handleInputChange}
+              row
+            >
+              <FormControlLabel
+                key="yes"
+                value="yes"
+                control={<Radio size="small" />}
+                label="yes"
+              />
+              <FormControlLabel
+                key="no"
+                value="no"
+                control={<Radio size="small" />}
+                label="no"
+              />
+              <FormControlLabel
+                key="maybe"
+                value="maybe"
+                control={<Radio size="small" />}
+                label="Other"
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+        <Button variant="contained" color="primary" type="submit">
+          Submit
+        </Button>
+      </Grid>
+    </form>
+  );
+};
