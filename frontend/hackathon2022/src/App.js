6e3f792fff76a08f13895461adf09912356fe86c
ShafinRez <shafinrezwan123@gmail.com>
2022-01-15 22:01:52 -0700

    sidebar

--- frontend/hackathon2022/src/App.js
+++ frontend/hackathon2022/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Calendar from './components/calendar'
+import Sidebar from './components/Sidebar';
 function App() {
   return (
     <div className='container'>
+      <Sidebar/>
       <div className="calendar">
         <Calendar/>
-
+        
       </div>
     </div>
     
---
+++ frontend/hackathon2022/src/components/SidebarData.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import HomeIcon from '@mui/icons-material/Home';
+import AddIcon from '@mui/icons-material/Add';
+export const SidebarData = [
+    {
+        title: "Home",
+        icon: <HomeIcon/>,
+        link: '/home'
+    },
+    {
+        title: "Add",
+        icon: <AddIcon/>,
+        link: ''
+    }
+
+
+
+]
+
+export default SidebarData
