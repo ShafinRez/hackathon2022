a139a09299f6855a4671c2f871252252947535ac
MAHIMA DHAWAN <mdhawan@ualberta.ca>
2022-01-16 09:04:20 -0700

    calender + the add form button

--- frontend/hackathon2022/src/App.js
+++ frontend/hackathon2022/src/App.js
@@ -2,13 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Calendar from './components/calendar'
 import Sidebar from './components/Sidebar';
+import  UserForm from './userForm';
+import CustomizedDialogs from './CustomizedDialogs'; 
+import React from 'react'; 
+
 function App() {
   return (
     <div className='container'>
       <Sidebar/>
+      <CustomizedDialogs>
+            <UserForm/>
+      </CustomizedDialogs>
       <div className="calendar">
         <Calendar/>
-        
       </div>
     </div>
     
---
+++ frontend/hackathon2022/src/userForm.js
@@ -0,0 +1,104 @@
+//reference: https://onestepcode.com/creating-a-material-ui-form/
+
+import React, { useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import Slider from "@material-ui/core/Slider";
+import Button from "@material-ui/core/Button";
+import './App.css';
+
+
+const defaultValues = {
+  task: "",
+  numberofHours: 0,
+  priority: "",
+};
+
+export default function UserForm(){
+  const [formValues, setFormValues] = useState(defaultValues);
+  const handleInputChange = (e) => { const { name, value } = e.target;
+                                        setFormValues({
+                                            ...formValues,
+                                            [name]: value,
+                                        });
+                                        };
+
+  const handleSliderChange = (name) => (e, value) => {
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formValues);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+    <div >
+      <Grid >
+        <Grid item class = "formtextName">
+          <TextField 
+            id="task-input"
+            name="task"
+            label="Task Name"
+            type="text"
+            value={formValues.task}
+            onChange={handleInputChange}
+          />
+        </Grid>
+        <Grid item>
+          <TextField 
+            id="numberofHours-input"
+            name="numberofHours"
+            label="Number of hours required"
+            type="number"
+            value={formValues.numberofHours}
+            onChange={handleInputChange}
+          />
+        </Grid>
+        <Grid item>
+          <FormControl >
+            <FormLabel class = "formtextName">Is it a Priority? </FormLabel>
+            <RadioGroup
+              name="priority"
+              value={formValues.priority}
+              onChange={handleInputChange}
+              row
+            >
+              <FormControlLabel 
+                key="yes"
+                value="yes"
+                control={<Radio size="small" />}
+                label="yes"
+              />
+              <FormControlLabel
+                key="no"
+                value="no"
+                control={<Radio size="small" />}
+                label="no"
+              />
+              <FormControlLabel
+                key="maybe"
+                value="maybe"
+                control={<Radio size="small" />}
+                label="Other"
+              />
+            </RadioGroup>
+          </FormControl>
+        </Grid>
+        <Button variant="contained" color="primary" type="submit">
+          Submit
+        </Button>
+      </Grid>
+      </div>
+    </form>
+  );
+};
