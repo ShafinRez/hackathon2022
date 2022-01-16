3368904a2e1334adf10c69c4b6c05b2abde815e8
byrontung <btung1@ualberta.ca>
2022-01-16 11:00:04 -0700

    Added background a some css for homepage.

--- frontend/hackathon2022/src/App.js
+++ frontend/hackathon2022/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Button } from '@mui/material';
 import './App.css';
-import Login from './components/Signup';
 import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-        <h1>HOME SCREEN</h1>
+        <div className='background'>
+          <div className='navbar'>
+            <img src="assets\logo.png" alt="Panda Logo" className='logo'/>
+            <div>
+              <Button type='button' href="/login" variant='outlined' className='button'>Login</Button>
+              <div className='divider'/>
+              <Button type='button' href="/signup" variant='outlined' className='button'>Signup</Button>
+            </div>
+            
+          </div>
+        </div>
       <nav>
         <Link to="/login"> Login </Link>
         <Link to="/signup"> Signup </Link>
